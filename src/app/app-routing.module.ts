e7db7f0252eea6c2fb5b6a4698fbffbe7ced35bf
arthurrmp <arthurrmp97@gmail.com>
2021-03-25 18:02:00 -0300

    remove "home" from the page url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full'
   },
 ];
