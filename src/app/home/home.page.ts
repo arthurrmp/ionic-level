36cce11d14244046786df68ffc6d6d4d9a6a2b6e
arthurrmp <arthurrmp97@gmail.com>
2021-03-25 17:39:56 -0300

    Code cleanup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { Motion, OrientationListenerEvent } from '@capacitor/motion';
-import { IonButton } from '@ionic/angular';
+import { AlertController, IonButton } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,27 +12,26 @@ export class HomePage implements AfterViewInit {
 
   @ViewChild('permission') buttonPermission: IonButton;
   public motion: OrientationListenerEvent;
-
   public angle: number = 0;
   public precision: number = 0.8;
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   async ngAfterViewInit() {
 
-    console.log(document.getElementsByClassName("permission"));
-
-    document.getElementsByClassName("permission")[0].addEventListener('click', async () => {
+    document.getElementById("permission").addEventListener('click', async () => {
       try {
         await DeviceMotionEvent.requestPermission();
       } catch (e) {
-        // Handle error
-        this.motion = e;
-        //return;
+        const alert = await this.alertController.create({
+          subHeader: 'We have not been able to access your device sensors 😞',
+          message: 'Your device may not support the DeviceMotion Web API',
+          buttons: ['OK']
+        });
+        await alert.present();
+        return;
       }
 
-
-      // Once the user approves, can start listening:
       Motion.addListener('orientation', (event: OrientationListenerEvent) => {
         this.motion = event
 
@@ -41,57 +40,8 @@ export class HomePage implements AfterViewInit {
         } else {
           this.angle = Math.round(event.beta);
         }
-
-
-        /* if (!(this.motion.beta < -43 || this.motion.beta > 43)) {
-          this.motion.beta = 43;
-        } */
-
-
       });
-
-      /* var ball = document.querySelector<HTMLElement>('.ball') ;
-      var garden = document.querySelector<HTMLElement>('.garden');
-      var output = document.querySelector<HTMLElement>('.output');
-
-      var maxX = garden.clientWidth - ball.clientWidth;
-      var maxY = garden.clientHeight - ball.clientHeight;
-
-      function handleOrientation(event) {
-        var x = event.beta;  // In degree in the range [-180,180)
-        var y = event.gamma; // In degree in the range [-90,90)
-
-        output.textContent = `beta : ${x}\n`;
-        output.textContent += `gamma: ${y}\n`;
-
-        // Because we don't want to have the device upside down
-        // We constrain the x value to the range [-90,90]
-        if (x > 90) { x = 90 };
-        if (x < -90) { x = -90 };
-
-        // To make computation easier we shift the range of
-        // x and y to [0,180]
-        x += 90;
-        y += 90;
-
-        // 10 is half the size of the ball
-        // It center the positioning point to the center of the ball
-        ball.style.top = (maxY * y / 180 - 10) + "px";
-        ball.style.left = (maxX * x / 180 - 10) + "px";
-      }
-
-      //window.addEventListener('deviceorientation', handleOrientation);
-      Motion.addListener('orientation', handleOrientation)
- */
-
-
     });
-
-
-
-    /*  window.addEventListener('devicemotion', (event) => {
-       this.motion = event.acceleration.x + ' m/s2';
-     }) */
   }
 
 
